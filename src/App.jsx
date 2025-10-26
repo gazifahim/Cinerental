0a750e8fe673052152d1eb84e5bf675748a98f1f
GAZI <gazifaahim@gmail.com>
2025-10-26 19:58:46 +0600

    created sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import Header from './Header'
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   return (
-  <Header />
-  )
+    <>
+      <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
 }

0f67d35debe7ec9fd079965d9588b9c28c9e85cc
GAZI <gazifaahim@gmail.com>
2025-10-26 23:57:55 +0600

    created movielist

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
 
 export default function App() {
   return (
     <>
       <Header />
       <main>
-        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-14">
           <Sidebar />
+          <MovieList />
         </div>
       </main>
     </>
